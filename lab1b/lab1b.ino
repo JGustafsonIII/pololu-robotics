91ceebdf1fa8410eda56be1c7dbea6851e711939
John Gustafson <John.Gustafson1@Marist.edu>
2021-02-20 10:44:13 -0500

    Some changes to acceleration

--- lab1b/lab1b.ino
+++ lab1b/lab1b.ino
@@ -2,14 +2,14 @@
 
 using namespace Pololu3piPlus32U4;
 
-Encoders encoders; 
+Encoders encoders;
 Buzzer buzzer;
 Motors motors;
 ButtonA buttonA;
 ButtonB buttonB;
 LCD lcd;
 
-unsigned long currentMillis; 
+unsigned long currentMillis;
 unsigned long prevMillis;
 
 // Represents the amount of time we want to wait in milliseconds
@@ -25,7 +25,7 @@ const float GEAR_RATIO = 75.81F;
 const float WHEEL_DIAMETER = 3.2;
 const float WHEEL_CIRCUMFERENCE = 10.0531;
 
-float foot = 30.5F; // 1ft
+float foot = 30.5F;           // 1ft
 float sixInches = (foot / 2); // 6inch
 float threeFeet = (foot * 3); // 3ft
 
@@ -35,10 +35,12 @@ float Sr = 0.0F;
 boolean finished = false;
 boolean taskComplete = false;
 int task = 0;
+
 /*
   Setup is only run once when it comes to the arduino.
 */
-void setup() {
+void setup()
+{
   Serial.begin(57600);
   delay(1000);
   buzzer.play("c32");
@@ -52,110 +54,176 @@ void setup() {
   You should basically picture the method as a loop itself so every few millseconds
   this will run checkEncoders() as an example.
 */
-void loop() {
-
- if (finished == false){
+void loop()
+{
+  if (finished == false)
+  {
     checkEncoders();
-    
-    if (task == 0) {
+
+    if (task == 0)
+    {
       taskComplete = moveForeward(foot);
-      if (taskComplete == true) {
+      if (taskComplete == true)
+      {
         task = 1;
         taskComplete = false;
       }
-    } else if (task == 1) {
+    }
+    else if (task == 1)
+    {
       taskComplete = moveBackward(-sixInches);
-      if (taskComplete == true) {
+      if (taskComplete == true)
+      {
         taskComplete = false;
         task = 2;
       }
-    } else if (task == 2) {
+    }
+    else if (task == 2)
+    {
       taskComplete = moveForeward(threeFeet);
-      if (taskComplete == true) {
+      if (taskComplete == true)
+      {
         taskComplete = false;
         task = 3;
         finished = true;
       }
     }
-  } else {
-    Serial.println("Tasks Completed");
-    exit(0);
   }
-} 
+  else
+  {
+    endProgram();
+    Serial.println("Tasks Completed");
+  }
+}
+
+void endProgram() {
+  buzzer.play("c32");
+  while(1); // Easy way of stopping the program
+}
 
 /**
  * Used for testing purposed to switch between running and not running.
  * @returns true if finished = false and false if finished = true
  */
-boolean onOffSwitch() {
-  if (finished == false) {
-    motors.setSpeeds(0,0);
+boolean onOffSwitch()
+{
+  if (finished == false)
+  {
+    motors.setSpeeds(0, 0);
     return true;
-  } else {
-    return false;  
+  }
+  else
+  {
+    return false;
   }
 }
 
-boolean moveForeward(float distance) {
-    int wheelSpeed = 300;
-    if(Sl < distance && Sr < distance) {
-      
-      if(Sl > (distance * .75)) {
-         wheelSpeed = 100 * ((distance - Sr) / 10);
-         if (wheelSpeed < 30) wheelSpeed = 30;
-      } else if (Sl > (distance * .50)) {
-         wheelSpeed = 200 * ((distance - Sr) / 10);
-      } else if (Sl > (distance * .25)) {
-         wheelSpeed = 300 * ((distance - Sr) / 10);
-      }
-      
-      motors.setSpeeds(wheelSpeed, wheelSpeed);
-    } else {
-      motors.setSpeeds(0, 0);  
-      Sl = 0.0F;
-      Sr = 0.0F;
-      delay(2000);
-      return true;
-    }
-    return false;
+/**
+ * Used to reset the robots speed and set a delay 
+*/
+void finishedTask(int wait)
+{
+  motors.setSpeeds(0, 0);
+  Sl = 0.0F;
+  Sr = 0.0F;
+  delay(wait);
 }
 
-// TODO: NEED TO GET WORKING
-boolean moveBackward(float distance) {  
-    int wheelSpeed = -400;
-    
-    if(Sl > distance && Sr > distance) {
-      
-      if(Sl < (distance * .75)) {
-         wheelSpeed = 100 * ((distance - Sr) / 10);
-         if (wheelSpeed < 30) wheelSpeed = -30;
-      } else if (Sl < (distance * .50)) {
-         wheelSpeed = 200 * ((distance - Sr) / 10);
-      } else if (Sl < (distance * .25)) {
-         wheelSpeed = 300 * ((distance - Sr) / 10);
-      }
-      
-      motors.setSpeeds(wheelSpeed, wheelSpeed);
-    } else {
-      motors.setSpeeds(0, 0);  
-      Sl = 0.0F;
-      Sr = 0.0F;
-      delay(2000);
-      return true;
+/**
+ * Moves the robot forwards based off the distance passed in
+ * @param distance The distance you want the robot to travel in metric (Ex. 1ft - 30.5cm)
+ * @returns Returns true if succesful and false if something went wrong
+*/
+boolean moveForeward(float distance)
+{
+  int wheelSpeed = 300;
+  if (Sl < distance && Sr < distance)
+  {
+
+    if (Sl > (distance * .75))
+    {
+      wheelSpeed = 50 * ((distance - Sr) / 10);
+      if (wheelSpeed < 30)
+        wheelSpeed = 30;
     }
-    return false;
+    else if (Sl > (distance * .50))
+    {
+      wheelSpeed = 100 * ((distance - Sr) / 10);
+    }
+    else if (Sl > (distance * .25))
+    {
+      wheelSpeed = 200 * ((distance - Sr) / 10);
+    }
+    else if (Sl < (distance *.25))
+    {
+      wheelSpeed = 100 * ((distance - Sr) / 10);
+    }
+
+    motors.setSpeeds(wheelSpeed, wheelSpeed);
+  }
+  else
+  {
+    finishedTask(2000);
+    return true;
+  }
+  return false;
 }
 
+/**
+ * Moves the robot backwards based off the distance passed in
+ * @param distance The distance you want the robot to travel in metric (Ex. 1ft - 30.5cm)
+ * @returns Returns true if succesful and false if something went wrong
+*/
+boolean moveBackward(float distance)
+{
+  int wheelSpeed = -300;
 
-void checkEncoders() {
+  if (Sl > distance && Sr > distance)
+  {
+    if (Sl < (distance * .75))
+    {
+      wheelSpeed = 50 * ((distance - Sr) / 10);
+      if (wheelSpeed < 30)
+        wheelSpeed = -30;
+    }
+    else if (Sl < (distance * .50))
+    {
+      wheelSpeed = 100 * ((distance - Sr) / 10);
+    }
+    else if (Sl < (distance * .25))
+    {
+      wheelSpeed = 200 * ((distance - Sr) / 10);
+    }
+    else if (Sl > (distance *.25))
+    {
+      wheelSpeed = 100 * ((distance - Sr) / 10);
+    }
+
+    motors.setSpeeds(wheelSpeed, wheelSpeed);
+
+  }
+  else
+  {
+    finishedTask(2000);
+    return true;
+  }
+  return false;
+}
+
+/**
+ * Used to calculate the distance on the wheels and print them into the serial monitor/plotter
+*/
+void checkEncoders()
+{
   currentMillis = millis();
-  if (currentMillis > prevMillis + PERIOD) {
+  if (currentMillis > prevMillis + PERIOD)
+  {
     countsLeft += encoders.getCountsAndResetLeft();
     countsRight += encoders.getCountsAndResetRight();
 
     Sl += ((countsLeft - prevLeft) / (CLICKS_PER_ROTATION * GEAR_RATIO) * WHEEL_CIRCUMFERENCE);
     Sr += ((countsRight - prevRight) / (CLICKS_PER_ROTATION * GEAR_RATIO) * WHEEL_CIRCUMFERENCE);
-    
+
     Serial.print("Left: ");
     Serial.print(Sl);
     Serial.print(" Right: ");
